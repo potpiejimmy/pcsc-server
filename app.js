cca47fc406249f7810dd6a1c8048415a44d6dcb1
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2016-10-01 23:58:51 +0200

    BCSDM-1 status and card endpoints

--- app.js
+++ app.js
@@ -22,48 +22,60 @@ pcsc.on('reader', function(r) {
             if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
                 console.log("card removed"); /* card removed */
                 reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
+                notifier.notifyObservers('status',
+                  {"severity":"error", "summary":"Status", "detail":"Card removed."});
             } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
                 console.log("card inserted"); /* card inserted */
-                reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
-                    if (err) console.log(err); 
-                    else readData(protocol);
-                });
+                notifier.notifyObservers('status',
+                  {"severity":"success", "summary":"Status", "detail":"Card inserted."});
             }
         }
     });
 });
 
-app.get('/', function(req, res) {
-    if (!reader) {
-        res.send("Reader not connected.");
-    } else {
+app.get('/status', function(req, res) {
+    if (!reader) res.send("Reader not connected.");
+    else {
         notifier.addObserver('status', function(arg) {
             res.send(arg);
         });
     }
 });
 
+app.get('/card', function(req, res) {
+    if (!reader) res.send("Reader not connected.");
+    else {
+        readData(function(data) {
+            res.send(data);
+        })
+    }
+});
+
 function sendAndReceive(protocol, data, callback) {
     reader.transmit(new Buffer(data,'hex'), 40, protocol, function(err, data) {
-        if (err) {
-            console.log(err);
-        } else {
+        if (err) console.log(err);
+        else {
             console.log('Data received', data);
             callback(data);
         }
     });
 }
 
-function readData(protocol) {
-    console.log('Protocol(', reader.name, '):', protocol);
-    // SELECT AID MAESTRO: '00A4040007A000000004306000'
-    sendAndReceive(protocol, '00A4040007A000000004306000', function(data) {
-        // GET_PROCESSING_OPTIONS: '80A8000002830000'
-        sendAndReceive(protocol, '80A8000006830408260826', function(data) {
-            // READ_RECORD: '00B2020C00'
-            sendAndReceive(protocol, '00B2010C00', function(data) {
-                notifier.notifyObservers('status', data.toString('hex'));
+function readData(callback) {
+    reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
+        if (err) console.log(err); 
+        else {
+            console.log('Protocol(', reader.name, '):', protocol);
+            // SELECT AID MAESTRO: '00A4040007A000000004306000'
+            sendAndReceive(protocol, '00A4040007A000000004306000', function(data) {
+                // GET_PROCESSING_OPTIONS: '80A8000002830000'
+                sendAndReceive(protocol, '80A8000006830408260826', function(data) {
+                    // READ_RECORD: '00B2020C00'
+                    sendAndReceive(protocol, '00B2010C00', function(data) {
+                        callback(data.toString('hex'));
+                    });
+                });
             });
-        });
+        }
     });
 }
