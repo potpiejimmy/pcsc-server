0071c381e323849381505e9bf76c3e2ac4537864
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2016-10-04 20:17:53 +0200

    BCSDM-1 moved pcsc and emv code into separate module

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var express = require('express');
-var pcsclite = require('pcsclite');
-var emv = require('node-emv');
 
+var pcsc = require('./pcsc-emv');
 var Notifier = require('./notifier');
 
 var app = express();
-var pcsc = pcsclite();
-
-var reader;
 var notifier = new Notifier();
 
 app.use(function(req, res, next) {
@@ -20,32 +16,19 @@ app.listen(3004, function () {
   console.log('Startup complete. Listening on port 3004.');
 });
 
-pcsc.on('reader', function(r) { 
-    console.log('New reader detected', r.name);
-    reader = r;
-
-    r.on('status', function(status) {
-        var changes = this.state ^ status.state;
-        if (changes) {
-            if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
-                console.log("card removed"); /* card removed */
-                reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
-                notifier.notifyObservers('status',
-                  {"severity":"warn", "summary":"Status", "detail":"Card removed."});
-            } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
-                console.log("card inserted"); /* card inserted */
-                notifier.notifyObservers('status',
-                  {"severity":"success", "summary":"Status", "detail":"Card inserted."});
-
-                // for testing:
-//                dumpMaestro();
-            }
-        }
-    });
-});
+pcsc.registerReader(
+    function() {
+        notifier.notifyObservers('status',
+        {"severity":"success", "summary":"Status", "detail":"Card inserted."});
+    },
+    function() {
+        notifier.notifyObservers('status',
+        {"severity":"warn", "summary":"Status", "detail":"Card removed."});
+    }
+);
 
 app.get('/status', function(req, res) {
-    if (!reader) res.send({"severity":"error", "summary":"Error", "detail":"Card reader not connected."});
+    if (!pcsc.getReader()) res.send({"severity":"error", "summary":"Error", "detail":"Card reader not connected."});
     else {
         notifier.addObserver('status', function(arg) {
             res.send(arg);
@@ -54,9 +37,9 @@ app.get('/status', function(req, res) {
 });
 
 app.get('/card', function(req, res) {
-    if (!reader) res.send({"severity":"error", "summary":"Error", "detail":"Card reader not connected."});
+    if (!pcsc.getReader()) res.send({"severity":"error", "summary":"Error", "detail":"Card reader not connected."});
     else {
-        readMaestro(function(tag57) {
+        pcsc.readMaestro(function(tag57) {
             if (tag57) {
                 res.send({
                     blz:   tag57.value.substr(0,8),
@@ -71,100 +54,3 @@ app.get('/card', function(req, res) {
         })
     }
 });
-
-function findEmvTag(emvData, tagName) {
-    var found;
-    emvData.forEach(function(tag) {
-        if (tag.tag == tagName) found=tag;
-    });
-    return found;
-}
-
-function sendAndReceive(protocol, data, callback) {
-    reader.transmit(new Buffer(data,'hex'), 512, protocol, function(err, data) {
-        if (err) console.log(err);
-        else {
-            console.log('Data received', data.toString('hex'));
-            callback(data);
-        }
-    });
-}
-
-function readMaestro(callback) {
-    reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
-        if (err) console.log(err); 
-        else {
-            // SELECT AID MAESTRO: '00A4040007A000000004306000'
-            sendAndReceive(protocol, '00A4040007A000000004306000', function(data) {
-                // EC
-                readRecord(protocol, 1, 3, 4, function(tag57) {
-                    if (tag57) {
-                        reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
-                        callback(tag57);
-                    } else {
-                        // try another one:
-                        readRecord(protocol, 2, 1, 6, function(tag57) {
-                            reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
-                            callback(tag57);
-                        });
-                    }
-                })
-
-            });
-        }
-    });
-}
-
-function readRecord(protocol, sfi, rec, offset, callback) {
-    sendAndReceive(protocol, '00B2'+hexChar(rec)+hexChar((sfi << 3) | 4)+'00', function(data) {
-        emv.parse(data.toString('hex').substr(offset), function(emvData) {
-            if (emvData != null) {
-                console.log(emvData);
-                callback(findEmvTag(emvData, '57'));
-            } else {
-                callback(null);
-            }
-        });
-    });
-}
-
-function hexChar(x) {
-    return ('0'+x.toString(16)).substr(-2);
-}
-
-// -------------- THE FOLLOWING IS FOR TESTING ONLY ------------
-
-function dumpNext(protocol, sfi, rec, maxSfi, maxRec) {
-    if (rec == 1) console.log("SFI " + sfi);
-    sendAndReceive(protocol, '00B2'+hexChar(rec)+hexChar((sfi << 3) | 4)+'00', function(data) {
-        if (sfi == maxSfi && rec == maxRec) {
-            reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
-        } else {
-            if (rec == maxRec) {rec = 1; sfi++;} else rec++;
-            dumpNext(protocol, sfi, rec, maxSfi, maxRec);
-        }
-    });
-}
-
-function dumpPSE() {
-    // SELECT 1PAY.SYS.DDF01 or 2PAY.SYS.DDF01 (contactless)
-    dumpAll(new Buffer("1PAY.SYS.DDF01", 'ASCII').toString('hex'));
-}
-
-function dumpMaestro() {
-    dumpAll("A0000000043060");
-}
-
-function dumpAll(dfname) {
-    reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
-        if (err) console.log(err); 
-        else {
-            console.log('Protocol(', reader.name, '):', protocol);
-            // SELECT DFNAME
-            sendAndReceive(protocol, '00A40400' + hexChar(dfname.length/2) + dfname +'00', function(data) {
-                // READ ALL
-                dumpNext(protocol, 1, 1, 3, 16);
-            });
-        }
-    });
-}
---
+++ pcsc-emv.js
@@ -0,0 +1,134 @@
+var pcsclite = require('pcsclite');
+var emv = require('node-emv');
+
+var pcsc = pcsclite();
+
+var reader;
+
+function registerReader(onCardInserted, onCardRemoved) {
+    pcsc.on('reader', function(r) { 
+        console.log('New reader detected', r.name);
+        reader = r;
+
+        r.on('status', function(status) {
+            var changes = this.state ^ status.state;
+            if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
+                console.log("card removed"); /* card removed */
+                reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
+                onCardRemoved();
+            } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
+                console.log("card inserted"); /* card inserted */
+                onCardInserted();
+                // for testing:
+//                dumpMaestro();
+            }
+        });
+    });
+}
+
+function getReader() {
+    return reader;
+}
+
+function findEmvTag(emvData, tagName) {
+    var found;
+    emvData.forEach(function(tag) {
+        if (tag.tag == tagName) found=tag;
+    });
+    return found;
+}
+
+function sendAndReceive(protocol, data, callback) {
+    reader.transmit(new Buffer(data,'hex'), 512, protocol, function(err, data) {
+        if (err) console.log(err);
+        else {
+            console.log('Data received', data.toString('hex'));
+            callback(data);
+        }
+    });
+}
+
+function readMaestro(callback) {
+    reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
+        if (err) { console.log(err); callback(); } 
+        else {
+            // SELECT AID MAESTRO: '00A4040007A000000004306000'
+            sendAndReceive(protocol, '00A4040007A000000004306000', function(data) {
+                // EC
+                readRecord(protocol, 1, 3, 4, function(tag57) {
+                    if (tag57) {
+                        reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
+                        callback(tag57);
+                    } else {
+                        // try another one:
+                        readRecord(protocol, 2, 1, 6, function(tag57) {
+                            reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
+                            callback(tag57);
+                        });
+                    }
+                })
+
+            });
+        }
+    });
+}
+
+function readRecord(protocol, sfi, rec, offset, callback) {
+    sendAndReceive(protocol, '00B2'+hexChar(rec)+hexChar((sfi << 3) | 4)+'00', function(data) {
+        emv.parse(data.toString('hex').substr(offset), function(emvData) {
+            if (emvData != null) {
+                console.log(emvData);
+                callback(findEmvTag(emvData, '57'));
+            } else {
+                callback(null);
+            }
+        });
+    });
+}
+
+function hexChar(x) {
+    return ('0'+x.toString(16)).substr(-2);
+}
+
+// -------------- THE FOLLOWING IS FOR TESTING ONLY ------------
+
+function dumpNext(protocol, sfi, rec, maxSfi, maxRec) {
+    if (rec == 1) console.log("SFI " + sfi);
+    sendAndReceive(protocol, '00B2'+hexChar(rec)+hexChar((sfi << 3) | 4)+'00', function(data) {
+        if (sfi == maxSfi && rec == maxRec) {
+            reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
+        } else {
+            if (rec == maxRec) {rec = 1; sfi++;} else rec++;
+            dumpNext(protocol, sfi, rec, maxSfi, maxRec);
+        }
+    });
+}
+
+function dumpPSE() {
+    // SELECT 1PAY.SYS.DDF01 or 2PAY.SYS.DDF01 (contactless)
+    dumpAll(new Buffer("1PAY.SYS.DDF01", 'ASCII').toString('hex'));
+}
+
+function dumpMaestro() {
+    dumpAll("A0000000043060");
+}
+
+function dumpAll(dfname) {
+    reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
+        if (err) console.log(err); 
+        else {
+            console.log('Protocol(', reader.name, '):', protocol);
+            // SELECT DFNAME
+            sendAndReceive(protocol, '00A40400' + hexChar(dfname.length/2) + dfname +'00', function(data) {
+                // READ ALL
+                dumpNext(protocol, 1, 1, 3, 16);
+            });
+        }
+    });
+}
+
+// ----------------------------------------
+
+module.exports.getReader = getReader;
+module.exports.registerReader = registerReader;
+module.exports.readMaestro = readMaestro;
