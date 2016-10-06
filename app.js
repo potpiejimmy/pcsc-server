fccc2742a6488bc48fc2c07b11eeb30efb1abb06
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2016-10-06 14:56:59 +0200

    BCSDM-1 added more specific card values for bank demo

--- app.js
+++ app.js
@@ -39,22 +39,22 @@ app.get('/status', function(req, res) {
 app.get('/card', function(req, res) {
     if (!pcsc.getReader()) res.send({"severity":"error", "summary":"Error", "detail":"Card reader not connected."});
     else {
-        // WINDOWS PLATFORM ONLY, ADD ARTIFICIAL DELAY OF 0.5s
-//        setTimeout(function() {
+        // WINDOWS PLATFORM ONLY, ADD ARTIFICIAL DELAY OF 0.75s
+        setTimeout(function() {
         pcsc.readMaestro(function(tag57) {
             if (tag57) {
                 res.send({
-                    blz:   tag57.value.substr(0,8),
-                    ktonr: tag57.value.substr(8,10)
+                    routingcode:  tag57.value.substr(3,5),
+                    branch:       tag57.value.substr(5,3),
+                    account:      tag57.value.substr(8,10),
+                    shortaccount: tag57.value.substr(9,7),
+                    subaccount:   tag57.value.substr(16,2),
                 });
             } else {
-                res.send({
-                    blz:   "Nicht gefunden",
-                    ktonr: "Nicht gefunden"
-                });
+                res.send({});
             }
         })
-        // WINDOWS PLATFORM ONLY, ADD ARTIFICIAL DELAY OF 0.5s
-//        }, 500);
+        // WINDOWS PLATFORM ONLY, ADD ARTIFICIAL DELAY OF 0.75s
+        }, 750);
     }
 });
